aa000a537d0c4f809f895c53b3b8be769f9d8d95
Leul Abera <liulabera43@gmail.com>
2025-10-15 10:11:27 +0300

    lib

--- module5/package/test.py
+++ module5/package/test.py
@@ -1,11 +1,13 @@
 # test.py inside package
 from .maths import *
+from package.maths import *
 from .subpackages.mult import *
+from package.subpackages.mult import *
 
 print(addition(2, 3))
 print(subtraction(5, 2))
 print(multiplication(3, 4))
 
 print("All imports worked correctly!")
-
+ 
 
